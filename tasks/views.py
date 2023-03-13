0ab404bc9231587ab319276b1bc7a55d48ed96b3
Jonathan Tinoco <tinocojona@outlook.com>
2023-03-13 07:26:49 -0600

    manejo de errores en sign_up

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -14,6 +15,24 @@ def sign_up(request):
             'form': UserCreationForm
         })
     elif request.method == "POST":
-        print("Hola")
-        print(request.POST)
-        return redirect('sign_up')
+        if request.POST['password1'] == request.POST['password2']:
+            # Register user
+            try:
+                username = request.POST['username']
+                password = request.POST['password1']
+                new_user = User.objects.create_user(
+                    username=username, password=password)
+                new_user.save()
+                return HttpResponse('User created successfully')
+            except:
+                return render(request, 'signup.html', {
+                    'title': "Sign Up",
+                    'form': UserCreationForm,
+                    'error': "Username already exists"
+                })
+        else:
+            return render(request, 'signup.html', {
+                'title': "Sign Up",
+                'form': UserCreationForm,
+                'error': 'Password does not match'
+            })
