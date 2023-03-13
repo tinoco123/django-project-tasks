6480314430ed673ba1e05c3c531fa2af1b7675d6
Jonathan Tinoco <tinocojona@outlook.com>
2023-03-13 08:54:02 -0600

    authentication finished

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('sign_up/', views.sign_up, name='sign_up')
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('login', views.sign_in, name='login'),
+    path('log_out', views.log_out, name='log_out')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'title': 'Home'
+    })
 
 
 def sign_up(request):
-    if request.method == "GET":
+    if request.method == 'GET':
         return render(request, 'signup.html', {
             'title': "Sign Up",
-            'form': UserCreationForm
+            'form': UserCreationForm,
         })
     elif request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
@@ -23,16 +27,50 @@ def sign_up(request):
                 new_user = User.objects.create_user(
                     username=username, password=password)
                 new_user.save()
-                return HttpResponse('User created successfully')
-            except:
+                login(request, new_user)
+                return redirect('tasks')
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'title': "Sign Up",
                     'form': UserCreationForm,
-                    'error': "Username already exists"
+                    'error': "Username already exists",
                 })
         else:
             return render(request, 'signup.html', {
                 'title': "Sign Up",
                 'form': UserCreationForm,
-                'error': 'Password does not match'
+                'error': 'Password does not match',
             })
+
+
+def tasks(request):
+    return render(request, 'tasks.html', {
+        'title': 'Tasks'
+    })
+
+
+def log_out(request):
+    logout(request)
+    return redirect('index')
+
+
+def sign_in(request):
+    if request.method == 'GET':
+        return render(request, 'sign_in.html', {
+            'title': 'Sign In',
+            'form': AuthenticationForm
+        })
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return render(request, 'sign_in.html', {
+                'title': 'Sign In',
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
