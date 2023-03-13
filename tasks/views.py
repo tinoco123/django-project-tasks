215b7edcf0e4f9a3118e82922eace29fbd030acb
Jonathan Tinoco <tinocojona@outlook.com>
2023-03-13 06:56:56 -0600

    working sign up functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
-def hello_world(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def index(request):
+    return render(request, 'index.html')
+
+
+def sign_up(request):
+    if request.method == "GET":
+        return render(request, 'signup.html', {
+            'title': "Sign Up",
+            'form': UserCreationForm
+        })
+    elif request.method == "POST":
+        print("Hola")
+        print(request.POST)
+        return redirect('sign_up')
